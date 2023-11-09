6c11a439cad4955248a1deaaee3857c4a457fe22
Alif Merchant <amerch04@uoguelph.ca>
2023-11-09 17:41:10 -0500

    Update docker images

---
+++ react/src/components/GlobalNavBar/index.js
@@ -0,0 +1,17 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+const GlobalNavBar = (props) => {
+    return <div>
+        <Navbar className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand>Notes App</Navbar.Brand>
+                <div>
+                    {props.pageName}
+                </div>
+            </Container>
+            
+        </Navbar>
+    </div>
+}
+
+export default GlobalNavBar;
--- react/src/pages/HomePage/index.js
+++ react/src/pages/HomePage/index.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Stack from 'react-bootstrap/Stack';
-import Modal from 'react-bootstrap/Modal';
-import Alert from 'react-bootstrap/Alert';
+import NotesList from './components/NotesList';
+import NewNoteForm from './components/NewNoteForm';
+import ErrorAlert from '../../components/ErrorAlert';
+import LoadingSpinner from '../../components/LoadingSpinner';
+import EditNoteModal from './components/EditNoteModal';
+import GlobalNavBar from '../../components/GlobalNavBar';
 
 
 const HomePage = () => {
@@ -109,104 +107,24 @@ const HomePage = () => {
 
     return(
         <div>
-            <Navbar className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand>Notes</Navbar.Brand>
-                </Container>
-            </Navbar>
+            <GlobalNavBar pageName='Home'/>
             <Container>
                 {error ? 
-                    <div>
-                        <Alert variant='danger' onClose={() => setError(false)} dismissible>
-                            {errorMessage}
-                        </Alert>
-                        <br/>
-                    </div>
+                    <ErrorAlert errorMessage={errorMessage} onClose={() => setError(false)} />
                     :
                     <div></div>
                 }
-                <Row>
-                    <h2>New Note</h2>
-                </Row>
-                <Row>
-                    <Stack direction="horizontal" gap={3}>
-                        <Form.Control as="textarea" rows={3} placeholder="Note..." value={newNote} onChange={(e) => setNewNote(e.target.value)}/>
-                        <Button onClick={() => addNewNote()}>Add Note</Button>
-                    </Stack>
-                </Row>
+                <NewNoteForm value={newNote} onChange={setNewNote} onSubmit={addNewNote} />
                 <br />
-                <Row>
-                    <h2>Note List</h2>
-                </Row>
                 {loading ? 
-                    <div>Loading...</div>
+                    <LoadingSpinner />
                     : 
-                    notes.length === 0? 
-                        <div>List is empty..</div>
-                        :
-                        <Row>
-                            <Stack gap={2}>
-                                {notes.map(note => {
-                                    return (
-                                        <Card key={note.id} bg="dark" text='white'>
-                                            <Card.Body>
-                                                <Stack direction="horizontal" gap={1}>
-                                                    <div className="p-2">{note.text}</div>
-                                                    <div className="p-2 ms-auto"><Button variant='danger' onClick={() => deleteNote(note.id)}>Delete</Button></div>
-                                                    <div className="p-2"><Button onClick={() => triggerEditModal(note)}>Edit</Button></div>
-                                                </Stack>
-                                            </Card.Body>
-                                        </Card>
-                                    );
-                                })}
-                            </Stack>
-                        </Row>
+                    <NotesList list={notes} onEdit={triggerEditModal} onDelete={deleteNote}/>
                 }
             </Container>
-            {EditModal(noteToEdit, showEdit, editClose, editNote)}
-            
+            <EditNoteModal note={noteToEdit} show={showEdit} onClose={editClose} onSave={editNote} />
         </div>
     )
-}
-
-const EditModal = (note, show, onClose, onSave) => {
-    const [noteText, setNoteText] = useState();
-    useEffect(() => {
-        if (note) {
-            setNoteText(note.text);
-        }
-    }, [note]);
-    return(
-        <Modal
-            show={show}
-            onHide={onClose}
-            backdrop="static"
-            keyboard={false}
-        >
-            <Modal.Header closeButton>
-            <Modal.Title>Edit Note</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form.Control as="textarea" rows={3} value={noteText} onChange={(e) => setNoteText(e.target.value)}/>
-            </Modal.Body>
-            <Modal.Footer>
-            <Button variant="secondary" onClick={onClose}>
-                Close
-            </Button>
-            <Button 
-                variant="primary" 
-                onClick={() => onSave(
-                    {
-                        ...note,
-                        text: noteText
-                    }
-                )}>
-                    Save
-            </Button>
-            </Modal.Footer>
-        </Modal>
-    )
-    
 }
 
 export default HomePage;
