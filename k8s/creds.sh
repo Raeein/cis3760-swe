d58fc5d17e930aa1594cf8768af73dd5e6388910
alif <alifmerchant86@gmail.com>
2023-11-13 17:37:09 -0500

    Added kubernetes + Ingress yamls

---
+++ k8s/creds.sh
@@ -0,0 +1,1 @@
+kubectl apply -f .\regcred.yaml
---
+++ k8s/setup.sh
@@ -0,0 +1,6 @@
+kubectl apply -f .\mysql\mysql-deployment.yaml
+kubectl apply -f .\mysql\mysql-service.yaml 
+kubectl apply -f .\spring\spring-deployment.yaml
+kubectl apply -f .\spring\spring-service.yaml
+kubectl apply -f .\react\react-deployment.yaml
+kubectl apply -f .\react\react-service.yaml
