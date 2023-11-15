c836030169f8e40601fecc7e27fe14fc1d0e4c30
alif <alifmerchant86@gmail.com>
2023-11-14 19:14:21 -0500

    Updated gitlab ci cd + kubernetes cluster manifests

--- k8s/setup.sh
+++
@@ -1,6 +0,0 @@
-kubectl apply -f .\mysql\mysql-deployment.yaml
-kubectl apply -f .\mysql\mysql-service.yaml 
-kubectl apply -f .\spring\spring-deployment.yaml
-kubectl apply -f .\spring\spring-service.yaml
-kubectl apply -f .\react\react-deployment.yaml
-kubectl apply -f .\react\react-service.yaml
