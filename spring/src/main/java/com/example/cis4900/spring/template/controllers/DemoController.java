d81ac88b837106bafb4d5565d107e788f21127a4
alif <alifmerchant86@gmail.com>
2023-12-02 13:25:03 -0500

    Updated ping path

--- spring/src/main/java/com/example/cis4900/spring/template/controllers/DemoController.java
+++ spring/src/main/java/com/example/cis4900/spring/template/controllers/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/demo")
+@RequestMapping(path = "/api")
 public class DemoController {
 
     @GetMapping()
