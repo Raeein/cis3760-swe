1456cc76000ceb25e39034cc9afcfe2b47e8fec7
alif <alifmerchant86@gmail.com>
2023-12-05 00:47:18 -0500

    Added new blog + minor code changes

--- mysql/scripts/setup.sql
+++ mysql/scripts/setup.sql
@@ -2,7 +2,7 @@ use template_db;
 
 create table if not exists note
 (
-    id   int auto_increment comment 'Primary Key'
+    id int auto_increment comment 'Primary Key'
         primary key,
     text varchar(255) null
 );
