c3a6430061e8191f39ed90a175a560ea6ceaaa88
alif <alifmerchant86@gmail.com>
2023-12-11 22:53:26 -0500

    Updated React Verison

--- react/src/components/GlobalNavBar/index.js
+++ react/src/components/GlobalNavBar/index.js
@@ -1,15 +1,18 @@
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 const GlobalNavBar = (props) => {
     return <div>
         <Navbar className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand>Notes App</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/blogs/">Blogs</Nav.Link>
+                </Nav>
                 <div>
                     {props.pageName}
                 </div>
             </Container>
-            
         </Navbar>
     </div>
 }
