2839fe42021abdf088b8e34dbe5c7492c8028ee0
Raeein Bagheri <raeein@aol.com>
2024-02-03 15:54:04 -0500

    added the scraper service template

---
+++ scraper/main.py
@@ -0,0 +1,29 @@
+import mariadb
+import sys
+import os
+
+user = os.getenv('DB_USER', 'default_user')
+password = os.getenv('DB_PASSWORD', 'default_password')
+host = os.getenv('DB_ADDRESS', 'default_host')
+port = int(os.getenv('DB_PORT', 'default_port'))
+database = os.getenv('DB_DATABASE', 'default_database')
+
+try:
+    conn = mariadb.connect(
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+        database=database
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+print("Connected to MariaDB Platform!")
+cur = conn.cursor()
+
+for i in range(10):
+    cur.execute("INSERT INTO note (text) VALUES (?)", (f"Hello, World! {i}",))
+
+conn.commit()
