8117f5e04045b1a14ef71204f870b8cd49319cf0
lee-ranaweer <lee.ranaweer@gmail.com>
2024-02-07 03:27:59 -0500

    Springboot

---
+++ react/springboot/src/main/java/pixelate/reactfrontend/ReactFrontEndApplication.java
@@ -0,0 +1,13 @@
+package pixelate.reactfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactFrontEndApplication.class, args);
+	}
+
+}
---
+++ react/springboot/src/test/java/pixelate/reactfrontend/ReactFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pixelate.reactfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
