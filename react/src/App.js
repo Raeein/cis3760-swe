b52a37f5069896debbb771919b3226b41ae3ec6b
lee-ranaweer <lee.ranaweer@gmail.com>
2024-02-07 05:12:35 -0500

    Docker react template

---
+++ react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JobList from './JobList';
+
+function App() {
+  return (
+    <div className="App">
+      <JobList />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/src/components/index.js
@@ -0,0 +1,15 @@
+//Can delete if no dependencies
+import React, { useState, useEffect } from 'react';
+
+
+const JobList = () => {
+    //Variables
+    
+    //Methods
+
+    return(
+        //PLACE HTML CODE FOR JOB LISTINGS HERE
+    )
+}
+
+export default JobList;
