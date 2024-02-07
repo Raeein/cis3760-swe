8431b7c4dc4866c178451b60b2934e37961fc308
Justin Parisio <justinparisio@Justins-MacBook-Pro.local>
2024-02-07 16:39:42 -0500

    changes for server

--- react/src/App.js
+++ react/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
 import './App.css';
-import JobList from './JobList';
+import JobTable from './components/JobTable.jsx'
+import jobDataTest from './test.json'
 
 function App() {
+
+  const [jobData, setJobData] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:8080/api/jobs/all')
+      .then((response) => response.json())
+      .then((data) => setJobData(data))
+      .catch((error) => console.error('Error:', error))
+  }, [])
+
+  console.log(jobData);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <JobList />
-      </header>
+      <JobTable data = {jobDataTest} />
     </div>
   );
 }
