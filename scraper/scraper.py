47593521b2a026ac5ac2e90bfebe298f755e66ad
theonej203 <lijayn516@gmail.com>
2024-02-10 22:22:23 -0500

    modify main.py to be able to use scraped data

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,80 +6,63 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# websites = ["https://ca.indeed.com/jobs?q={jobTitle}&l={location}"]
 
-# webHome = ["https://ca.indeed.com"]
-
-# jobInfoTags = ["job_seen_beacon"]
-
-
-jobBoardList = [
+job_board_list = [
                     {
-                        "jobBoardName": "Indeed",
-                        "jobBoardBaseUrl": "https://ca.indeed.com", 
-                        "jobBoardSearchUrl": "https://ca.indeed.com/jobs?q={jobTitle}&l={location}",
+                        "job_board_name": "Indeed",
+                        "job_board_base_url": "https://ca.indeed.com", 
+                        "job_board_search_url": "https://ca.indeed.com/jobs?q={job_title}&l={location}",
 
-                        "jobCardElement": "div",
-                        "jobCardClass": "job_seen_beacon",
+                        "job_card_element": "div",
+                        "job_card_class": "job_seen_beacon",
 
-                        "jobTitleElement": "h1",
-                        "jobTitleSearchObject": {"class": "jobsearch-JobInfoHeader-title"},
+                        "job_title_element": "h1",
+                        "job_title_search_object": {"class": "jobsearch-JobInfoHeader-title"},
 
-                        "jobCompanyElement": "div",
-                        "jobCompanySearchObject": {"data-company-name": "true"},
+                        "job_company_element": "div",
+                        "job_company_search_object": {"data-company-name": "true"},
 
-                        "jobLocationElement": "div",
-                        "jobLocationSearchObject": {"data-testid": "inlineHeader-companyLocation"},
+                        "job_location_element": "div",
+                        "job_location_search_object": {"data-testid": "inlineHeader-companyLocation"},
 
-                        "jobEmploymentElement": "div",
-                        "jobEmploymentSearchObject": {"aria-label":"Job type"},
+                        "job_employment_element": "div",
+                        "job_employment_search_object": {"aria-label":"Job type"},
 
-                        "jobPayElement": "div",
-                        "jobPaySearchObject": {"aria-label":"Pay"},
+                        "job_pay_element": "div",
+                        "job_pay_search_object": {"aria-label":"Pay"},
 
-                        "jobDescriptionElement": "div",
-                        "jobDescriptionSearchObject": {"id":"jobDescriptionText"}
+                        "job_description_element": "div",
+                        "job_description_search_object": {"id":"jobDescriptionText"}
                     }
                 ]
 
 
-def getJobInfo(jobTitle:str, location:str, specifiedJobBoards:list[str] = [] ):
-
-    
-    
+def getJobInfo(job_title:str, location:str, specified_job_boards:list[str] = [] ):
+    #return variable
+    job_json_object_list = []
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-blink-features=AutomationControlled")
-
     #https://www.whatismybrowser.com/guides/the-latest-user-agent/chrome seem to be working
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36")
-
-
+    options.add_argument("""user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36""")
     service = Service(ChromeDriverManager().install())
 
     driver = webdriver.Chrome(service=service, options=options)
-
     driver.implicitly_wait(10)
 
 
-    jobJsonObjectList = []
-
-    jobBoardSearchList = []
-    if(specifiedJobBoards == []):
-        jobBoardSearchList = jobBoardList
+    job_board_search_list = []
+    if(specified_job_boards == []):
+        job_board_search_list = job_board_list
     else:
-        for jobBoard in specifiedJobBoards:
-            for jobBoardObject in jobBoardList:
-                if(jobBoard["jobBoardName"] == jobBoard):
-                    jobBoardSearchList.append(jobBoardObject)
+        for job_board in specified_job_boards:
+            for job_board_object in job_board_list:
+                if(job_board["job_board_name"] == job_board):
+                    job_board_search_list.append(job_board_object)
                     break
-
-
         
-    for jobBoardObject in jobBoardSearchList:
-    
-    # url = websites[0].format(jobTitle=jobTitle, location=location)
-        url = jobBoardObject["jobBoardSearchUrl"].format(jobTitle=jobTitle, location=location)
+    for job_board_object in job_board_search_list:
+        url = job_board_object["job_board_search_url"].format(job_title=job_title, location=location)
         
         time.sleep(2)
         driver.implicitly_wait(10)
@@ -88,61 +71,66 @@ def getJobInfo(jobTitle:str, location:str, specifiedJobBoards:list[str] = [] ):
 
         soup = BeautifulSoup(driver.page_source, "html.parser")
 
+        job_cards = soup.find_all(job_board_object["job_card_element"], class_=job_board_object["job_card_class"] )
 
-        # jobUrl = webHome[0]+soup.find_all("div", class_=jobInfoTags[0] )[0].find('a', href=True)['href']
-        jobCards = soup.find_all(jobBoardObject["jobCardElement"], class_=jobBoardObject["jobCardClass"] )
+        for job_card in job_cards:
 
-        for jobCard in jobCards:
-
-            jobUrl = jobBoardObject["jobBoardBaseUrl"]+jobCard.find('a', href=True)['href']
-            jobJsonObject = {}
+            jobUrl = job_board_object["job_board_base_url"]+job_card.find('a', href=True)['href']
+            job_json_object = {}
 
             time.sleep(2)
             driver.implicitly_wait(10)
             driver.get(url=jobUrl)
             
             
-            jobSoup = BeautifulSoup(driver.page_source, "html.parser")
+            job_soup = BeautifulSoup(driver.page_source, "html.parser")
 
             print("-"*20)
-            title = jobSoup.find(jobBoardObject["jobTitleElement"], jobBoardObject["jobTitleSearchObject"]).text
-            jobJsonObject["title"] = title
+            title = job_soup.find(job_board_object["job_title_element"], 
+                                 job_board_object["job_title_search_object"]).text
+            job_json_object["title"] = title
             print("title: ", title)
-            
 
-            company = jobSoup.find(jobBoardObject["jobCompanyElement"], jobBoardObject["jobCompanySearchObject"]).text
-            jobJsonObject["company"] = company
+            company = job_soup.find(job_board_object["job_company_element"], 
+                                   job_board_object["job_company_search_object"]).text
+            job_json_object["company"] = company
             print("company: ", company)
 
-            location = jobSoup.find(jobBoardObject["jobLocationElement"], jobBoardObject["jobLocationSearchObject"]).text
-            jobJsonObject["location"] = location
+            location = job_soup.find(job_board_object["job_location_element"], 
+                                    job_board_object["job_location_search_object"]).text
+            job_json_object["location"] = location
             print("location:", location)
 
             try:
-                employmentType = jobSoup.find(jobBoardObject["jobEmploymentElement"], jobBoardObject["jobEmploymentSearchObject"]).text[8:]
+                employment_type = job_soup.find(job_board_object["job_employment_element"], 
+                                              job_board_object["job_employment_search_object"]).text[8:]
             except:
-                employmentType = "Unknown"
+                employment_type = "Unknown"
             finally:
-                jobJsonObject["employment_type"] = employmentType
-                print("employment type: ", employmentType)
+                job_json_object["employment_type"] = employment_type
+                print("employment type: ", employment_type)
 
             try:
-                salary = jobSoup.find(jobBoardObject["jobPayElement"], jobBoardObject["jobPaySearchObject"]).text[3:]
+                salary = job_soup.find(job_board_object["job_pay_element"], 
+                                      job_board_object["job_pay_search_object"]).text[3:]
             except:
                 salary = "Unknown"
             finally:
-                jobJsonObject["salary"] = salary
+                job_json_object["salary"] = salary
                 print("salary: ",salary)
 
-            description = jobSoup.find(jobBoardObject["jobDescriptionElement"], jobBoardObject["jobDescriptionSearchObject"]).text
-            jobJsonObject["description"] = description
+            description = job_soup.find(job_board_object["job_description_element"], 
+                                       job_board_object["job_description_search_object"]).text
+            job_json_object["description"] = description
             print("description: ", description)
 
             print(jobUrl)
             print("-"*20)
-            jobJsonObjectList.append(jobJsonObject)
+            job_json_object_list.append(job_json_object)
 
 
-    return jobJsonObjectList
+    return job_json_object_list
 
-print(getJobInfo("Software","Toronto"))
+
+if __name__ == "__main__":
+    getJobInfo("Software Developer", "Toronto, On")
