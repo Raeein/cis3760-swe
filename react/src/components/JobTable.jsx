09dc5e85a87aa216f69f3c68dc6f23c2596c3da4
Justin Parisio <justinparisio@Justins-M3-MacBook-Pro.local>
2024-03-09 16:22:09 -0500

    added filter side panel and option for salary and employment type, neither are functional yet - just a set up

--- react/src/components/JobTable.jsx
+++ react/src/components/JobTable.jsx
@@ -5,11 +5,30 @@ import "react-sliding-pane/dist/react-sliding-pane.css";
 
 export default function JobTable({ data }) {
   const [search, setSearch] = useState("");
-  const [detailsPane, setDetailsPane] = useState({ visible: false, data: null });
+  const [salary, setSalary] = useState({ min: 0, max: 200000 });
+  const [detailsPane, setDetailsPane] = useState({
+    visible: false,
+    data: null,
+  });
+  const [filterPane, setfilterPane] = useState({
+    visible: false,
+    data: null,
+  });
 
   function searchBarChange(event) {
     setSearch(event.target.value);
   }
+
+  function handleMinSalaryChange(event) {
+    setSalary({ min: parseInt(event.target.value), max: salary.max });
+  }
+
+  function handleMaxSalaryChange(event) {
+    setSalary({ min: salary.min, max: parseInt(event.target.value) });
+  }
+
+  // console.log(salary);
+  // console.log(data);
 
   return (
     <>
@@ -18,13 +37,22 @@ export default function JobTable({ data }) {
       </div>
 
       <div className="JobTable">
-        <SearchBar onSearched={searchBarChange} />
+        <div className="search-container">
+          <SearchBar onSearched={searchBarChange} />
+          <button
+            className="filter-icon"
+            onClick={() => setfilterPane({ visible: true, data: null })}
+          >
+            filter
+          </button>
+        </div>
         <div className="card-list">
           {data
             .filter((item) => {
               return (
                 search.toLowerCase() === "" ||
-                item.jobTitle.toLowerCase().includes(search.toLowerCase())
+                item.jobTitle.toLowerCase().includes(search.toLowerCase()) ||
+                item.jobLocation.toLowerCase().includes(search.toLowerCase())
               );
             })
             .map((job) => (
@@ -47,7 +75,7 @@ export default function JobTable({ data }) {
           isOpen={detailsPane.visible}
           title={detailsPane.data.jobTitle}
           onRequestClose={() => setDetailsPane({ visible: false, data: null })}
-          from="left"  
+          from="left"
           width="30%"
           overlayClassName="overlay"
           className="slider"
@@ -58,6 +86,62 @@ export default function JobTable({ data }) {
             <p>Company: {detailsPane.data.company}</p>
             <p>Description: {detailsPane.data.jobDescription}</p>
             {/* <p>{detailsPane.data}</p> */}
+          </div>
+        </SlidingPane>
+      )}
+
+      {filterPane.visible && (
+        <SlidingPane
+          isOpen={filterPane.visible}
+          title="Filter Options"
+          onRequestClose={() => setfilterPane({ visible: false, data: null })}
+          from="left"
+          width="30%"
+          overlayClassName="overlay"
+          className="slider"
+        >
+          <div className="slider-details">
+            <div className="filters-wrapper">
+              <div className="salary-wrapper">
+                <p>Salary</p>
+                <div className="values-wrapper">
+                  <input
+                    type="text"
+                    placeholder="min"
+                    onChange={handleMinSalaryChange}
+                  />
+                  <input
+                    type="text"
+                    placeholder="max"
+                    onChange={handleMaxSalaryChange}
+                  />
+                </div>
+              </div>
+              <div className="employment-wrapper">
+                <p>Employment Type</p>
+                <div className="employment-type-wrapper">
+                  <label htmlFor="full-time">
+                    <input
+                      type="checkbox"
+                      id="full-time"
+                      name="full-time"
+                      value="full-time"
+                    />
+                    Full Time
+                  </label>
+
+                  <label htmlFor="part-time">
+                    <input
+                      type="checkbox"
+                      id="part-time"
+                      name="part-time"
+                      value="part-time"
+                    />
+                    Part Time
+                  </label>
+                </div>
+              </div>
+            </div>
           </div>
         </SlidingPane>
       )}
