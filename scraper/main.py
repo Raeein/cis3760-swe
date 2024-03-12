6ff7d3376e6489df4813047663a40c0ca1785c05
guangli <guangli@uoguelph.ca>
2024-03-12 09:22:28 -0400

    apply poetry to python code

--- scraper/main.py
+++ scraper/main.py
@@ -12,15 +12,9 @@ host = os.getenv('DB_ADDRESS', 'default_host')
 port = int(os.getenv('DB_PORT', 'default_port'))
 database = os.getenv('DB_DATABASE', 'default_database')
 
-
-
 try:
     conn = mariadb.connect(
-        user=user,
-        password=password,
-        host=host,
-        port=port,
-        database=database
+        user=user, password=password, host=host, port=port, database=database
     )
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
@@ -30,37 +24,30 @@ print("Connected to MariaDB Platform!")
 cur = conn.cursor()
 
 insert_statement = """
-    INSERT INTO job (jobid, job_title, job_location, salary, job_description, company, employment_type)
+    INSERT INTO job (
+        jobid, job_title, job_location,
+        salary, job_description, company, employment_type
+    )
     VALUES (NULL, ?, ?, ?, ?, ?, ?);
-    """
-
-# uncomment the code below to use test data
-
-# with open("fake_jobs.json") as f:
-#     data = json.load(f)
-#     for job in data['jobs']:
-#         job_title = job["title"]
-#         job_location = job["location"]
-#         salary = job.get("salary", "Negotiable")  # Assuming 'salary' might not be present in all records
-
-#         res = cur.execute(insert_statement, (job_title, job_location, salary))
-#         if res == 0:
-#             print("Error inserting data: ", job_title, job_location, salary)
+"""
 
 
-while(True):
+while (True):
     print("Start job scraping session")
-    job_object_generator = scraper.get_job_info("Software Developer", "Toronto, ON", ["Canadian Job Bank", "Indeed"])
+    job_object_generator = scraper.get_job_info(
+        "Software Developer", "Toronto, ON", ["Canadian Job Bank", "Indeed"]
+    )
     job_object = next(job_object_generator)
-    while(job_object != None):
+    while (job_object is not None):
         res = scraper.insert_into_database(job_object, conn, cur)
         if res == 0:
-            print("Error inserting data: ", job_object["title"], job_object["location"], 
-                job_object["salary"], job_object["description"], job_object["company"], job_object["employment_type"])
+            print(
+                "Error inserting data: ", job_object["title"],
+                job_object["location"], job_object["salary"],
+                job_object["description"], job_object["company"],
+                job_object["employment_type"]
+            )
         job_object = next(job_object_generator)
 
     print("Finish scraping for this session. Next session starts in 1 hour")
-    # cur.execute("SELECT jobid, job_title, job_location, salary, company FROM job")
-    # for (jobid, job_title, job_location, salary, company) in cur:
-    #     print(f"Job: {jobid}, {job_title}, {job_location}, {salary}, {company}")
     time.sleep(3600)
--- scraper/test_scraper.py
+++ scraper/test_scraper.py
@@ -10,45 +10,60 @@ def mock_connection():
     return connection, cursor
 
 
-
 @pytest.mark.parametrize("file_location, job_board, length", [
     ("testWebsite/indeed/indeedJobBoard1.txt", "Indeed", 15),
     ("testWebsite/indeed/indeedJobBoard2.txt", "Indeed", 5),
     ("testWebsite/indeed/indeedJobBoard3.txt", "Indeed", 6),
     ("testWebsite/canadian_job/canadianJobBank.txt", "Canadian Job Bank", 25)
 ])
-def testGetJobCardFromIndeed1(file_location:str, job_board:str, length:int):
-
+def test_get_job_card(file_location: str, job_board: str, length: int):
     job_board_file_1 = open(file_location, "r", encoding="utf8")
-    job_cards = scraper.get_job_cards_from_html(job_board_file_1.read(), job_board)
+    job_cards = scraper.get_job_cards_from_html(
+        job_board_file_1.read(), job_board
+    )
     job_board_file_1.close
     assert len(job_cards) == length
 
 
-
-@pytest.mark.parametrize("file_location, job_board, title, company, location, employment_type, salary", [
-    (
-        "testWebsite/indeed/indeedJob1.txt", "Indeed", "Cloud Solutions Engineer - .NET and Azure", 
-        "Aviso Wealth", "151 Yonge Street, Toronto, ON", "Full Time", "$105,000–$117,000 a year"
-    ),
-    (
-        "testWebsite/indeed/indeedJob2.txt", "Indeed", "Junior Java Developer", 
-        "Triunity Software", "Toronto, ON", "Full Time", "$70,000 a year"
-    ),
-    (
-        "testWebsite/indeed/indeedJob3.txt", "Indeed", "Senior Software Engineer | Python Developer",
-        "Scotiabank", "40 King Street West, Toronto, ON", "Permanent", "Unknown"
-    ),
-    (
-        "testWebsite/canadian_job/CanadianJob1.txt", "Canadian Job Bank", "software engineer",
-        "Micharity Inc", "Toronto, ON", "Full Time,Permanent", "$52.88HOUR hourly /   40 hours per week"
-    )
-])
-def test_get_and_insert_job_data(file_location:str, job_board:str, title:str, company:str, location:str, employment_type:str, salary:str, mock_connection):
+@pytest.mark.parametrize(
+    """file_location, job_board, title, company,
+    location, employment_type, salary""", [
+        (
+            "testWebsite/indeed/indeedJob1.txt", "Indeed",
+            "Cloud Solutions Engineer - .NET and Azure",
+            "Aviso Wealth", "151 Yonge Street, Toronto, ON",
+            "Full Time", "$105,000–$117,000 a year"
+        ),
+        (
+            "testWebsite/indeed/indeedJob2.txt", "Indeed",
+            "Junior Java Developer",
+            "Triunity Software", "Toronto, ON",
+            "Full Time", "$70,000 a year"
+        ),
+        (
+            "testWebsite/indeed/indeedJob3.txt", "Indeed",
+            "Senior Software Engineer | Python Developer",
+            "Scotiabank", "40 King Street West, Toronto, ON",
+            "Permanent", "Unknown"
+        ),
+        (
+            "testWebsite/canadian_job/CanadianJob1.txt", "Canadian Job Bank",
+            "software engineer",
+            "Micharity Inc", "Toronto, ON",
+            "Full Time,Permanent", "$52.88HOUR hourly /   40 hours per week"
+        )
+    ]
+)
+def test_get_and_insert_job_data(
+    file_location: str, job_board: str, title: str, company: str,
+    location: str, employment_type: str, salary: str, mock_connection
+):
     connection, cursor = mock_connection
-    insert_statement = """INSERT INTO job (jobid, job_title, job_location, salary, job_description, company, employment_type)VALUES (NULL, ?, ?, ?, ?, ?, ?);"""
+    insert_statement = scraper.insert_statement
     job_file = open(file_location, "r", encoding="utf8")
-    job_json = scraper.get_job_json(job_file.read(), job_board, "https://dummy.url" )
+    job_json = scraper.get_job_json(
+        job_file.read(), job_board, "https://dummy.url"
+    )
     job_file.close()
 
     assert job_json["title"] == title
@@ -59,13 +74,16 @@ def test_get_and_insert_job_data(file_location:str, job_board:str, title:str, co
 
     res = scraper.insert_into_database(job_json, connection, cursor)
     assert res == 1
-    cursor.execute.assert_called_with(insert_statement, (job_json["title"], job_json["location"], job_json["salary"], job_json["description"], job_json["company"], job_json["employment_type"]))
-    
+    cursor.execute.assert_called_with(insert_statement, (
+        job_json["title"], job_json["location"],
+        job_json["salary"], job_json["description"],
+        job_json["company"], job_json["employment_type"]
+    ))
 
 
-def testGetWebDriver():
+def test_get_web_driver():
     driver = scraper.get_firefox_driver()
-    assert  driver != None
+    assert driver is not None
     scraper.stall_driver(driver)
     assert True
 
@@ -74,25 +92,64 @@ def testLoadTargetedJobBoard():
     targeted_job_board = scraper.load_targeted_job_board()
     assert targeted_job_board == ["Indeed", "Canadian Job Bank"]
 
-    targeted_job_board = scraper.load_targeted_job_board(["Canadian Job Bank",])
+    targeted_job_board = scraper.load_targeted_job_board(
+        ["Canadian Job Bank",]
+    )
     assert targeted_job_board == ["Canadian Job Bank",]
 
     targeted_job_board = scraper.load_targeted_job_board(["Indeed", ])
     assert targeted_job_board == ["Indeed", ]
 
+
 def testGetJobBoardSearchUrl():
-    url = scraper.get_job_board_search_url("Software Engineer", "Toronto", "Indeed")
-    assert url != None
+    url = scraper.get_job_board_search_url(
+        "Software Engineer", "Toronto", "Indeed"
+    )
+    assert url is not None
+
 
 def testGetIndeedJobUrl():
-    job_board_file_1 = open("testWebsite/indeed/indeedJobBoard1.txt", "r", encoding="utf8")
-    jobCards = scraper.get_job_cards_from_html(job_board_file_1.read(), "Indeed")
+    job_board_file_1 = open(
+        "testWebsite/indeed/indeedJobBoard1.txt", "r", encoding="utf8"
+    )
+    jobCards = scraper.get_job_cards_from_html(
+        job_board_file_1.read(), "Indeed"
+    )
+    indeed_link = """
+                    https://ca.indeed.com/pagead/clk
+                    ?mo=r&ad=-6NYlbfkN0DFRBgdkffDjRejVobbg8KVPSs6CgnXSfnYo3Qc-
+                    NFE2L-XKvK7g0tzAN47iE-7-6GDlOe0HPUmlFwR_W5ypPuLTdyMgC2RALO
+                    PVZz4DDdOBNFIt6a4mgwlZBRnyzfg1y22jsSY3BTy8gBYMrrjaAotockQp
+                    KfUEP2-fkF0cY_Qbc-2_hO1lIyEhDClCVFXclvMoxn0eihqFz_WUQyHnV4
+                    A0pD-MXULJUp0nmRFoj2TmngceU0STyyjbx-Ki7kwUBiFaddQ1efAo7w2j
+                    k_98bO3yOTZE-zRDilOMXBlAm5SdL99COtUackXtvf9t_d0YO7r1dlp6jr
+                    J65sU7TFY0r49PD3wYx0KhylThzOL9qqLHjvjM9hK2NBJJFrQDHsmzjGGm
+                    mUu5bREDSz2YkrLYYDg3zg-zTPcRyiycGMbZSNVe2tCUDyMSOAxB10mBuU
+                    QAtInt-9arhdbPO4Sgww2UqF3gz75EvPTnoZ-FEoAqGCGRltr7krCOf3NP
+                    rPgAAqSpj3163jqQJPd0ZBFK_9_kZuPPiDiZKAo-otjUX09Q7Lh11zlwHK
+                    h6t4lCGj-I_xFsflBfzYqOVH4SG1odvB72bgzIw9E7ygRqb2IOpcuiddBp
+                    zIZCg8XWmqCEq9h6H2KxtrPlOJSPaQ93sIz3Zp3kPbOKZzcZ_zEba7wSZI
+                    YRBt0ww==&xkcb=SoDW6_M3EPWoaZWQx50LbzkdCdPP&camk=4HOcmqOLY
+                    rCLTJoowOo4eQ==&p=0&fvj=1&vjs=3
+                """.replace("\n", "")
 
-    assert scraper.get_job_url("Indeed", jobCards[0]) == "https://ca.indeed.com/pagead/clk?mo=r&ad=-6NYlbfkN0DFRBgdkffDjRejVobbg8KVPSs6CgnXSfnYo3Qc-NFE2L-XKvK7g0tzAN47iE-7-6GDlOe0HPUmlFwR_W5ypPuLTdyMgC2RALOPVZz4DDdOBNFIt6a4mgwlZBRnyzfg1y22jsSY3BTy8gBYMrrjaAotockQpKfUEP2-fkF0cY_Qbc-2_hO1lIyEhDClCVFXclvMoxn0eihqFz_WUQyHnV4A0pD-MXULJUp0nmRFoj2TmngceU0STyyjbx-Ki7kwUBiFaddQ1efAo7w2jk_98bO3yOTZE-zRDilOMXBlAm5SdL99COtUackXtvf9t_d0YO7r1dlp6jrJ65sU7TFY0r49PD3wYx0KhylThzOL9qqLHjvjM9hK2NBJJFrQDHsmzjGGmmUu5bREDSz2YkrLYYDg3zg-zTPcRyiycGMbZSNVe2tCUDyMSOAxB10mBuUQAtInt-9arhdbPO4Sgww2UqF3gz75EvPTnoZ-FEoAqGCGRltr7krCOf3NPrPgAAqSpj3163jqQJPd0ZBFK_9_kZuPPiDiZKAo-otjUX09Q7Lh11zlwHKh6t4lCGj-I_xFsflBfzYqOVH4SG1odvB72bgzIw9E7ygRqb2IOpcuiddBpzIZCg8XWmqCEq9h6H2KxtrPlOJSPaQ93sIz3Zp3kPbOKZzcZ_zEba7wSZIYRBt0ww==&xkcb=SoDW6_M3EPWoaZWQx50LbzkdCdPP&camk=4HOcmqOLYrCLTJoowOo4eQ==&p=0&fvj=1&vjs=3"
+    assert scraper.get_job_url("Indeed", jobCards[0]) == indeed_link
+
 
 def testGetCanadianJobUrl():
-    job_board_file_4 = open("testWebsite/canadian_job/canadianJobBank.txt", "r", encoding="utf8")
-    jobCards = scraper.get_job_cards_from_html(job_board_file_4.read(), "Canadian Job Bank")
+    job_board_file_4 = open(
+        "testWebsite/canadian_job/canadianJobBank.txt", "r", encoding="utf8"
+    )
+    jobCards = scraper.get_job_cards_from_html(
+        job_board_file_4.read(), "Canadian Job Bank"
+    )
 
-    assert scraper.get_job_url("Canadian Job Bank", jobCards[0]) == "https://www.jobbank.gc.ca/jobsearch/jobposting/40322760;jsessionid=1B78D6117D1291E6CA832BF65BFBD84E.jobsearch74?source=searchresults"
+    canadian_job_link = """
+                            https://www.jobbank.gc.ca/jobsearch/jobposting/
+                            40322760;jsessionid=1B78D6117D1291E6CA832BF65BFB
+                            D84E.jobsearch74?source=searchresults
+                        """.replace("\n", "")
 
+    assert scraper.get_job_url(
+        "Canadian Job Bank", jobCards[0]
+    ) == canadian_job_link
