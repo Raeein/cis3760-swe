8575d8fe76309c021c299dc97d50ebfc08ad199d
Nelith Ranaweera <nranawee@uoguelph.ca>
2024-03-13 10:44:37 -0400

    Issue #176. Refactoring old code to be accepted

--- react/.eslintrc.js
+++ react/.eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
@@ -34,7 +35,7 @@ module.exports = {
         ],
         "linebreak-style": [
             "error",
-            "windows"
+            "unix"
         ],
         "quotes": [
             "error",
--- react/src/App.js
+++ react/src/App.js
@@ -5,23 +5,23 @@ import JobTable from "./components/JobTable.jsx";
 // import fakejobs from "./test.json";
 
 export default function App() {
-  const [jobData, setJobData] = useState([]);
-  useEffect(() => {
-    fetch("/api/jobs/all")
-      .then((response) => response.json())
-      .then((data) => setJobData(data))
-      .catch((error) => console.error("Error:", error));
-  }, []);
+    const [jobData, setJobData] = useState([]);
+    useEffect(() => {
+        fetch("/api/jobs/all")
+            .then((response) => response.json())
+            .then((data) => setJobData(data))
+            .catch((error) => console.error("Error:", error));
+    }, []);
 
-  // console log the infomration in the json file as a string
-  console.log(jobData);
+    // console log the infomration in the json file as a string
+    console.log(jobData);
 
-  return (
-    <div className="container">
-      <JobTable data={jobData} />
-      <div className="map">
-        <h1>MAP</h1>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <JobTable data={jobData} />
+            <div className="map">
+                <h1>MAP</h1>
+            </div>
+        </div>
+    );
 }
