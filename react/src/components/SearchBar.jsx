faee987989445aa65bf4af8898e4be4e25eb4d15
Justin Parisio <justinparisio@Justins-M3-MacBook-Pro.local>
2024-03-16 15:55:28 -0400

    added router

--- react/src/components/SearchBar.jsx
+++ react/src/components/SearchBar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar({ onSearched }) {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate("/search");
+    };
+
     return (
         <input
             className="search-bar"
             type="text"
             placeholder="search for a job..."
+            onClick={handleClick}
             onChange={onSearched}
         />
     );
