32f1fd63fd83af3818987d445fa755b268bdc9f4
Justin Parisio <justinparisio@Justins-M3-MacBook-Pro.local>
2024-03-28 20:26:22 -0400

    some styling work

--- react/src/components/job card/JobCard.jsx
+++ react/src/components/job card/JobCard.jsx
@@ -8,10 +8,26 @@ export default function JobCard({ job, handleDetailsPane }) {
             key={job.jobId}
             onClick={() => handleDetailsPane({ visible: true, data: job })}
         >
-            <h1>{job.jobTitle}</h1>
-            <p>Salary: {job.salary}</p>
-            <p>Company: {job.company}</p>
-            <p>located in {job.jobLocation}</p>
+            <div className="card-header">
+                <div className="header-wrapper">
+                    <div className="source-logo"></div>
+                    <div className="post-info">
+                        <p className="post-info-title">Indeed</p>
+                        <p className="post-info-posted">
+                            Posted {Math.floor(Math.random() * 30) + 1} days ago
+                        </p>
+                    </div>
+                    <div className="apply">
+                        {" "}
+                        <p>Apply</p>{" "}
+                    </div>
+                </div>
+            </div>
+            <div className="card-body">
+                <h1>{job.jobTitle}</h1>
+                <p>located in {job.jobLocation}</p>
+            </div>
+            <div className="card-footer"></div>
         </article>
     );
 }
